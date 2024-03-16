67e72e32f1061666e49f5deac2293b327becac05
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-16 17:23:42 +0900

    [feat] 임시 가게 정보 타임리프 문법 기반 할당

--- src/main/java/com/eight/sailingship/controller/TempController.java
+++ src/main/java/com/eight/sailingship/controller/TempController.java
@@ -1,12 +1,25 @@
 package com.eight.sailingship.controller;
 
+import com.eight.sailingship.entity.Store;
 import com.eight.sailingship.repository.TempRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class TempController {
 
     private final TempRepository tempRepository;
+
+    @GetMapping
+    public String stores(Model model) {
+        List<Store> stores = tempRepository.findAll();
+        model.addAttribute("stores", stores);
+
+        return "main.html";
+    }
 }
