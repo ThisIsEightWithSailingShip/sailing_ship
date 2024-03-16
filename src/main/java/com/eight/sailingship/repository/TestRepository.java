f0c5f1838dbb3ad04b4309f9f3ee4396a9a9dfdf
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-16 22:40:25 +0900

    [style] 겹치는 파일 삭제

--- src/main/java/com/eight/sailingship/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eight.sailingship.repository;
-
-import com.eight.sailingship.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Store, Long> {
-}
