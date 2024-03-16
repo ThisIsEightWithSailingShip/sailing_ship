ced0580fbaae3bd5900fc909361cc6d119baf88c
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-17 01:54:49 +0900

    [style] 매장 수정 폼 페이지 작성

--- src/main/java/com/eight/sailingship/controller/StoreController.java
+++ src/main/java/com/eight/sailingship/controller/StoreController.java
@@ -1,12 +1,11 @@
 package com.eight.sailingship.controller;
 
+import com.eight.sailingship.dto.store.StoreRequestDto;
 import com.eight.sailingship.service.store.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,7 +20,13 @@ public class StoreController {
     }
 
     @GetMapping("/{storeId}")
-    public String getStoreDetail(@PathVariable Long storeId, Model model){
+    public String getStore(@PathVariable Long storeId, Model model){
         return storeService.getStore(model, storeId);
+    }
+
+    @PutMapping
+    public String updateStore(Model model, @ModelAttribute StoreRequestDto requestDto) {
+        storeService.updateStore(model, requestDto);
+        return "redirect:/sail/store";
     }
 }
