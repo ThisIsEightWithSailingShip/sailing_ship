81e370234bd71cd009dadafe9f5c2e11b764df63
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-17 03:46:16 +0900

    [feat] 매장 수정 페이지 조회 경로 작성

--- src/main/java/com/eight/sailingship/controller/StoreController.java
+++ src/main/java/com/eight/sailingship/controller/StoreController.java
@@ -14,14 +14,23 @@ public class StoreController {
 
     private final StoreService storeService;
 
+    // 전체 매장 페이지 조회
     @GetMapping
     public String getStores(Model model) {
         return storeService.getStores(model);
     }
 
+    // 특정 매장 페이지 조회
     @GetMapping("/{storeId}")
     public String getStore(@PathVariable Long storeId, Model model){
         return storeService.getStore(model, storeId);
+    }
+
+    // 매장 수정 페이지 조회
+    @GetMapping("/{storeId}/update")
+    public String updateStore(@PathVariable Long storeId, Model model) {
+        // storeId의 값이 UserDetails 객체의 값에서 타고 타고 조회해야 함
+        return "store-update.html";
     }
 
     @PutMapping
