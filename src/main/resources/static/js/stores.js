22d6d48ca4b4ddc78e5eace9f25bb34ffced8d7f
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-16 22:28:38 +0900

    [feat] 페이지 전환 로직 필요

--- src/main/resources/static/js/stores.js
+++ src/main/resources/static/js/stores.js
@@ -1,9 +1,18 @@
 // document.querySelectorAll('.store-card').forEach(function(card) {
 //     card.addEventListener('click', function() {
-//         let storeId = card.getAttribute("storeId"); // 데이터 속성으로부터 storeId를 읽어옴
-//         // 페이지 이동
+//         let storeIdString = card.getAttribute("storeId"); // 데이터 속성으로부터 storeId를 읽어옴
+//         // console.log(storeId)
+//         let storeId = parseInt(storeIdString);
 //         console.log(storeId)
-//         let id = parseInt(storeId);
-//         window.location.href = `/sail/stores/${id}`;
+//         fetchStoreDetail(storeId); // 추출한 storeId를 사용하여 상세 정보를 가져옴
 //     });
 // });
+//
+// function fetchStoreDetail(storeId) {
+//     fetch(`/sail/store/${storeId}`)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data);
+//         })
+//         .catch(error => console.error('Error fetching store detail:', error));
+// }
