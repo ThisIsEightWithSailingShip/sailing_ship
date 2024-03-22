c7ae1e1e50351910cffc8d78aca4bab2445ddbd8
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-22 15:05:11 +0900

    [feat] 이메일 인증 UI 구현 완료

--- src/main/java/com/eight/sailingship/service/email/EmailService.java
+++ src/main/java/com/eight/sailingship/service/email/EmailService.java
@@ -44,7 +44,7 @@ public class EmailService {
         String toMail = email;
         String title = "회원 가입 인증 이메일 입니다."; // 이메일 제목
         String content =
-                "돛단배달의 민족에 가입해주셔서 감사합니다." + 	//html 형식으로 작성 !
+                        "돛단배달의 민족에 가입해주셔서 감사합니다." + 	//html 형식으로 작성 !
                         "<br><br>" +
                         "인증 번호는 " + authNumber + "입니다." +
                         "<br>" +
@@ -55,18 +55,22 @@ public class EmailService {
 
     //이메일을 전송합니다.
     public void mailSend(String setFrom, String toMail, String title, String content) {
-        MimeMessage message = mailSender.createMimeMessage();//JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
+        //JavaMailSender 객체를 사용하여 MimeMessage 객체를 생성
+        MimeMessage message = mailSender.createMimeMessage();
+
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");//이메일 메시지와 관련된 설정을 수행합니다.
+            // 이메일 메시지와 관련된 설정을 수행
             // true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
-            helper.setFrom(setFrom);//이메일의 발신자 주소 설정
-            helper.setTo(toMail);//이메일의 수신자 주소 설정
-            helper.setSubject(title);//이메일의 제목을 설정
-            helper.setText(content,true);//이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
+            MimeMessageHelper helper = new MimeMessageHelper(message,true,"utf-8");
+            helper.setFrom(setFrom); //이메일의 발신자 주소 설정
+            helper.setTo(toMail); //이메일의 수신자 주소 설정
+            helper.setSubject(title); //이메일의 제목을 설정
+            helper.setText(content,true); //이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정.
             mailSender.send(message);
-        } catch (MessagingException e) {//이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
+        } catch (MessagingException e) {
+            // 이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
             // 이러한 경우 MessagingException이 발생
-            e.printStackTrace();//e.printStackTrace()는 예외를 기본 오류 스트림에 출력하는 메서드
+            e.printStackTrace();
         }
 
 
