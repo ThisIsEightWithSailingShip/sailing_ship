736c506909b2a3e77c1463461b698502e08f2510
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-23 14:14:49 +0900

    [feat] 메시지 전송 메소드 채팅 컨트롤러 선언

---
+++ src/main/java/com/eight/sailingship/controller/ChatController.java
@@ -0,0 +1,19 @@
+package com.eight.sailingship.controller;
+
+import com.eight.sailingship.dto.chat.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage
+    ) {
+        return chatMessage;
+    }
+}
---
+++ src/main/java/com/eight/sailingship/dto/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.eight.sailingship.dto.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
