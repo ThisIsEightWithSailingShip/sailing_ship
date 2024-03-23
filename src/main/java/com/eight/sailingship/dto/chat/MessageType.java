eef8b431b305be7134ffa0a64df070c462c0def8
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-24 03:46:46 +0900

    [fix] 채팅 기능 드랍

--- src/main/java/com/eight/sailingship/dto/chat/MessageType.java
+++
@@ -1,7 +0,0 @@
-package com.eight.sailingship.dto.chat;
-
-public enum MessageType {
-    CHAT,
-    JOIN,
-    LEAVE
-}
--- src/main/java/com/eight/sailingship/service/chat/ChatService.java
+++
@@ -1,6 +0,0 @@
-package com.eight.sailingship.service.chat;
-
-public interface ChatService {
-    // 이제까지의 채팅 내역 저장
-    String save();
-}
