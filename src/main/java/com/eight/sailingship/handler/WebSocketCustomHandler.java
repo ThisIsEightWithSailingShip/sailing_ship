d86d259a555d52fbab0917d5487c840158c51d3b
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-23 17:47:02 +0900

    [feat] 세션 아이디 기반 메시지 전송 + 종료 알림 구현

--- src/main/java/com/eight/sailingship/handler/WebSocketCustomHandler.java
+++ src/main/java/com/eight/sailingship/handler/WebSocketCustomHandler.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 public class WebSocketCustomHandler extends TextWebSocketHandler {
 
+    // 레포지토리 대신 임시 해쉬맵 구조
     private final Map<String, WebSocketSession> sessions = new HashMap<>();
 
     // 최초 연결 시
@@ -35,13 +36,36 @@ public class WebSocketCustomHandler extends TextWebSocketHandler {
     //양방향 데이터 통신할 떄 해당 메서드가 call 된다.
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        //do something
+        final String sessionId = session.getId();
+
+        sessions.values().forEach((s) -> {
+            if (!s.getId().equals(sessionId) && s.isOpen()) {
+                try {
+                    s.sendMessage(message);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     //웹소켓 종료
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        final String sessionId = session.getId();
+        final String leaveMessage = sessionId + "님이 떠났습니다.";
+        sessions.remove(sessionId); // 삭제
 
+        // 종료 메시지 발신
+        sessions.values().forEach((s) -> {
+            if (!s.getId().equals(sessionId) && s.isOpen()) {
+                try {
+                    s.sendMessage(new TextMessage(leaveMessage));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     //통신 에러 발생 시
